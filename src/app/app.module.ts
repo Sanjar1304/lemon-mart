bc46d8fdd5ba28c3a4e40903787b0b4297da2dbf
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-05-15 11:20:29 +0500

    Added new custom modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { MaterialModule } from './material.module'
+import { InventoryModule } from './inventory/inventory.module'
+import { ManagerModule } from './manager/manager.module'
+import { MaterialModule } from './material.module';
+import { PosModule } from './pos/pos.module';
+import { UserModule } from './user/user.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
