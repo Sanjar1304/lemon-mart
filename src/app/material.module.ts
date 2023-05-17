45d9670f6038eb5d30f56b932b212302f82ffe8d
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-05-17 11:56:29 +0500

    Added material icon feature

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { MatIconModule } from '@angular/material/icon'
 import { MatToolbarModule } from '@angular/material/toolbar'
 
-const modules = [MatToolbarModule]
+const modules = [MatToolbarModule, MatIconModule]
 
 @NgModule({
   declarations: [],
