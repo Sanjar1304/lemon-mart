b886756df1e03bb9f30c97835ea1d7b3c925522f
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-05-17 12:11:33 +0500

    Styled navbar part of the material toolbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,8 +2,9 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { MatIconModule } from '@angular/material/icon'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
 
-const modules = [MatToolbarModule, MatIconModule]
+const modules = [MatToolbarModule, MatIconModule, MatButtonModule]
 
 @NgModule({
   declarations: [],
