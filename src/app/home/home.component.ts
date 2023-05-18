1459edc0abffe868f41a8d8d22096fd3cf8ae30e
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-05-18 11:10:46 +0500

    Updated home-component wit view and style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,30 +2,8 @@ import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-home',
-  template: `
-    <div class="home-container">
-      <span class="home-title">Hello, Limoncu !</span>
-      <button mat-raised-button color="primary">Login</button>
-    </div>
-  `,
-  styles: [
-    `
-      .home-container {
-        margin-top: 32px;
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        align-items: center;
-      }
-      .home-title {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-top: 2rem;
-        margin-bottom: 2rem;
-      }
-    `,
-  ],
+  templateUrl: './home.component.html',
+  styleUrls: ['home.component.css'],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
