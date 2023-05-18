a5beb36873492473ead067ff0d14a492e8d255f1
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-05-18 14:14:32 +0500

    Lazy-loading is finished

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,17 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
 
-import { ManagerRoutingModule } from './manager-routing.module';
-import { ManagerHomeComponent } from './manager-home/manager-home.component';
-
+import { ManagerHomeComponent } from './manager-home/manager-home.component'
+import { ManagerRoutingModule } from './manager-routing.module'
 
 @NgModule({
-  declarations: [
-    ManagerHomeComponent
-  ],
-  imports: [
-    CommonModule,
-    ManagerRoutingModule
-  ]
+  declarations: [ManagerHomeComponent],
+  imports: [CommonModule, ManagerRoutingModule],
 })
-export class ManagerModule { }
+export class ManagerModule {}
