22b8b6f722fcb14b736ce652e5635186558c0559
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-05-22 11:33:52 +0500

    Added manager and receipt-lookup c and m

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core'
 
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
 import { ManagerRoutingModule } from './manager-routing.module'
+import { ManagerComponent } from './manager.component'
+import { MaterialModule } from '../material.module';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component'
 
 @NgModule({
-  declarations: [ManagerHomeComponent],
-  imports: [CommonModule, ManagerRoutingModule],
+  declarations: [ManagerHomeComponent, ManagerComponent, UserManagementComponent, ReceiptLookupComponent],
+  imports: [CommonModule, ManagerRoutingModule, MaterialModule],
 })
 export class ManagerModule {}
