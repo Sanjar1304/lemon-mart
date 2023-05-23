1f26af1a152aa4c04b7ee51476dad1afeff0eba0
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-05-23 12:27:07 +0500

    Added pos component

--- src/app/manager/manager-routing.module.ts
+++ src/app/manager/manager-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
+import { ManagerComponent } from './manager.component'
 import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component'
 import { UserManagementComponent } from './user-management/user-management.component'
 
 const routes: Routes = [
   {
     path: '',
-    component: ManagerHomeComponent,
+    component: ManagerComponent,
     children: [
       { path: '', redirectTo: '/manager/home', pathMatch: 'full' },
       { path: 'home', component: ManagerHomeComponent },
