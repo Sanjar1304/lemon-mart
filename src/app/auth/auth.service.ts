d67109fd713088b962959dd82f403b09272f37c7
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-05-31 12:44:08 +0500

    Added abstract protected methods in auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { User } from '../user/user/user'
 import { Role } from './auth.enum'
 import { IAuthService } from './interfaces/IAuthService'
 import { IAuthStatus } from './interfaces/IAuthStatus'
+import { IServerAuthResponse } from './interfaces/IServerAuthResponse'
 
 export const defaultAuthStatus: IAuthStatus = {
   isAuthenticated: false,
@@ -17,6 +18,13 @@ export const defaultAuthStatus: IAuthStatus = {
 export abstract class AuthService implements IAuthService {
   readonly authStatus$ = new BehaviorSubject<IAuthStatus>(defaultAuthStatus)
   readonly currentUser$ = new BehaviorSubject<IUser>(new User())
+
+  protected abstract authProvider(
+    email: string,
+    password: string
+  ): Observable<IServerAuthResponse>
+  protected abstract transformJwtToken(token: unknown): IAuthStatus
+  protected abstract getCurrentUser(): Observable<User>
 
   constructor() {}
 
