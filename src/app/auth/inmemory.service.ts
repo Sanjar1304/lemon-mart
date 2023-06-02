831b5e1f2584feee86431a433c365682424f4043
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-02 14:36:48 +0500

    Methods overriden and added default user in s

--- src/app/auth/inmemory.service.ts
+++ src/app/auth/inmemory.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core'
 import { sign } from 'fake-jwt-sign'
 import { Observable, of, throwError } from 'rxjs'
 
+import { PhoneType } from '../user/user/EnumPhoneType'
 import { User } from '../user/user/user'
 import { Role } from './auth.enum'
 import { AuthService } from './auth.service'
@@ -10,13 +11,30 @@ import { IServerAuthResponse } from './interfaces/IServerAuthResponse'
 
 @Injectable()
 export class InMemoryAuthService extends AuthService {
-  defaultUser: any
-  protected override transformJwtToken(token: unknown): IAuthStatus {
-    throw new Error('Method not implemented.')
-  }
-  protected override getCurrentUser(): Observable<User> {
-    throw new Error('Method not implemented.')
-  }
+  private defaultUser = User.Build({
+    _id: '5da01751da27cc462d265913',
+    email: 'sanjarsuyunov1304@gmail.com',
+    name: { first: 'Sanjar', last: 'Suyunov' },
+    picture:
+      'https://media.licdn.com/dms/image/C5603AQFDBNkXAw-cgQ/profile-displayphoto-shrink_800_800/0/1632468017275?e=1691020800&v=beta&t=0atFdz2CRWOCoYE3E1Nzx8BdNQa6xLJCMIrj_fplabY',
+    role: Role.Manager,
+    dateOfBirth: new Date(1991, 13, 4),
+    userStatus: true,
+    address: {
+      line1: 'Olmazor, TashGU-TashPI, 5/36',
+      city: 'Tashkent',
+      state: 'Olmazor district',
+      zip: '100109',
+    },
+    level: 2,
+    phones: [
+      {
+        id: 0,
+        type: PhoneType.Mobile,
+        digits: '998977506980',
+      },
+    ],
+  })
 
   constructor() {
     super()
@@ -58,5 +76,13 @@ export class InMemoryAuthService extends AuthService {
       }),
     } as IServerAuthResponse
     return of(authResponse)
+  }
+
+  protected transformJwtToken(token: IAuthStatus): IAuthStatus {
+    return token
+  }
+
+  protected getCurrentUser(): Observable<User> {
+    return of(this.defaultUser)
   }
 }
